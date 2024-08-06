e14694960590a0393b26627dfcdd519446f3f955
apehex <apehex@protonmail.com>
2024-08-06 20:55:13 +0200

    Remove useless layer: encoder, blocks and decoder already have layer norms

--- revml/contract/decoder/model.py
+++ revml/contract/decoder/model.py
@@ -59,16 +59,15 @@ class Transformer(tf.keras.models.Model):
                 epsilon=epsilon,
                 name='block-{}'.format(__i))
             for __i in range(num_layers)]
-        self._norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, beta_initializer='zeros', gamma_initializer='ones', name='final-norm') # rms_scaling=True
         self._decoder = tokun.model.Decoder(token_dim=token_dim[::-1], encoding_dim=output_dim, embedding_dim=embed_dim, sequence_axis=1, feature_axis=-1, activation=activation, output=output, name='decoder')
 
     def call(self, inputs: tf.Tensor, attention_mask: tf.Tensor=None, **kwargs) -> tf.Tensor:
-        # byte embedding
+        # compress the inputs
         __y = self._encoder(inputs)
         # blocks
         __y = functools.reduce(lambda __x, __b: __b(inputs=__x, attention_mask=attention_mask, **kwargs), self._blocks, __y)
-        # normalize & decompress
-        return self._decoder(self._norm(__y))
+        # decompress
+        return self._decoder(__y)
 
     def get_config(self) -> dict:
         __config = super(Transformer, self).get_config()
