a85e61f589c1f91e70e43386038d59a419bb511e
apehex <apehex@protonmail.com>
2024-11-26 19:10:07 +0100

    Match the latest mlable API

--- revml/contract/layers.py
+++ revml/contract/layers.py
@@ -8,6 +8,7 @@ import mlable.blocks.transformer
 # CONSTANTS ###################################################################
 
 EPSILON = 1e-6
+DROPOUT = 0.0
 
 # ENCODER-DECODER #############################################################
 
@@ -15,11 +16,10 @@ EPSILON = 1e-6
 class DecoderBlock(tf.keras.layers.Layer):
     def __init__(
         self,
-        num_heads: int,
-        embed_dim: int,
-        head_dim: int,
-        hidden_dim: int,
-        sequence_axis: int=1,
+        head_num: int,
+        attention_axes: list=[1],
+        expand_rate: int=4,
+        dropout_rate: float=DROPOUT,
         epsilon: float=EPSILON,
         **kwargs
     ) -> None:
@@ -27,24 +27,47 @@ class DecoderBlock(tf.keras.layers.Layer):
         super(DecoderBlock, self).__init__(**kwargs)
         # config
         self._config = {
-            'num_heads': num_heads,
-            'embed_dim': embed_dim,
-            'head_dim': head_dim,
-            'hidden_dim': hidden_dim,
-            'sequence_axis': sequence_axis,
+            'head_num': head_num,
+            'attention_axes': [attention_axes] if isinstance(attention_axes, int) else list(attention_axes),
+            'expand_rate': int(expand_rate),
+            'dropout_rate': dropout_rate,
             'epsilon': epsilon,}
         # layers
-        self._self_attention = mlable.blocks.transformer.SelfAttentionBlock(num_heads=num_heads, head_dim=head_dim, sequence_axis=sequence_axis, epsilon=epsilon, center=False, scale=False)
-        self._cross_attention = mlable.blocks.transformer.CrossAttentionBlock(num_heads=num_heads, head_dim=head_dim, sequence_axis=sequence_axis, epsilon=epsilon, center=False, scale=False)
-        self._ffn = mlable.blocks.transformer.FeedForwardBlock(embed_dim=embed_dim, hidden_dim=hidden_dim, epsilon=epsilon, center=False, scale=False)
+        self._attend_cross = None
+        self._attend_self = None
+        # custom build
+        self._built = False
 
-    def build(self, inputs_shape: tf.TensorShape, contexts_shape: tf.TensorShape) -> None:
-        # propagate the shapes trhough the child layers
-        self._self_attention.build(inputs_shape)
-        self._cross_attention.build(inputs_shape=inputs_shape, contexts_shape=contexts_shape)
-        self._ffn.build(inputs_shape)
-        # register
-        self.built = True
+    def _build(self, inputs_shape: tuple, contexts_shape: tuple) -> None:
+        if not self._built:
+            __embed_dim = inputs_shape[-1]
+            __head_num = self._config['head_num']
+            __head_dim = __embed_dim // __head_num
+            __hidden_dim = __embed_dim * self._config['expand_rate']
+            # common args
+            __args = {
+                'head_num': __head_num,
+                'key_dim': __head_dim,
+                'value_dim': __head_dim,
+                'hidden_dim': __hidden_dim,
+                'attention_axes': self._config['attention_axes'],
+                'dropout_rate': self._config['dropout_rate'],
+                'epsilon': self._config['epsilon'],
+                'use_bias': True,
+                'center': True,
+                'scale': True,}
+            # init
+            self._attend_cross = mlable.blocks.transformer.ResidualDecoderBlock(**__args)
+            self._attend_self = mlable.blocks.transformer.ResidualDecoderBlock(**__args)
+            # propagate the shapes trhough the child layers
+            self._attend_cross._build(query_shape=inputs_shape, key_shape=contexts_shape, value_shape=contexts_shape)
+            self._attend_self._build(query_shape=inputs_shape, key_shape=inputs_shape, value_shape=inputs_shape)
+            # register
+            self.built, self._built = True, True
+
+    def build(self, inputs_shape: tuple, contexts_shape: tuple=None) -> None:
+        if contexts_shape is not None:
+            self._build(inputs_shape=inputs_shape, contexts_shape=contexts_shape)
 
     def call(
         self,
@@ -53,12 +76,11 @@ class DecoderBlock(tf.keras.layers.Layer):
         attention_mask: tf.Tensor=None,
         training: bool=False,
     ) -> tf.Tensor:
-        # residual + self attention
-        __x = inputs + self._self_attention(inputs=inputs, attention_mask=attention_mask, training=training, use_causal_mask=True)
-        # residual + cross attention
-        __x = __x + self._cross_attention(inputs=__x, contexts=contexts, attention_mask=None, training=training, use_causal_mask=False)
-        # residual + augment
-        return __x + self._ffn(__x)
+        self._build(inputs_shape=tuple(inputs.shape), contexts_shape=tuple(contexts.shape))
+        # residual + cross attention + FFN
+        __outputs = self._attend_cross(query=inputs, key=contexts, value=contexts, attention_mask=None, training=training, use_causal_mask=False)
+        # residual + self attention + FFN
+        return self._attend_self(query=__outputs, key=__outputs, value=__outputs, attention_mask=attention_mask, training=training, use_causal_mask=True)
 
     def get_config(self) -> dict:
         __config = super(DecoderBlock, self).get_config()
--- revml/contract/model.py
+++ revml/contract/model.py
@@ -12,6 +12,7 @@ import revml.contract.layers
 # CONSTANTS ###################################################################
 
 EPSILON = 1e-6
+DROPOUT = 0.0
 
 # ENCODING-DECODING ##############################################################
 
@@ -19,13 +20,13 @@ EPSILON = 1e-6
 class Transformer(tf.keras.models.Model):
     def __init__(
         self,
-        num_layers: int,
-        num_heads: int,
+        layer_num: int,
+        head_num: int,
         input_dim: int,
         context_dim: int,
         embed_dim: int,
-        head_dim: int,
-        hidden_dim: int,
+        expand_rate: int=4,
+        dropout_rate: float=DROPOUT,
         epsilon: float=EPSILON,
         **kwargs
     ) -> None:
@@ -33,56 +34,59 @@ class Transformer(tf.keras.models.Model):
         super(Transformer, self).__init__(**kwargs)
         # config
         self._config = {
-            'num_layers': num_layers,
-            'num_heads': num_heads,
+            'layer_num': layer_num,
+            'head_num': head_num,
             'input_dim': input_dim,
             'context_dim': context_dim,
             'embed_dim': embed_dim,
-            'head_dim': head_dim,
-            'hidden_dim': hidden_dim,
+            'expand_rate': expand_rate,
+            'dropout_rate': dropout_rate,
             'epsilon': epsilon,}
-        # the inputs is always UTF-32-BE bytes => 256
-        self._embed_input = mlable.layers.embedding.TokunEmbedding(input_dim=256, output_dim=embed_dim // input_dim, name='embed-inputs')
-        self._embed_context = mlable.layers.embedding.TokunEmbedding(input_dim=256, output_dim=embed_dim // context_dim, name='embed-contexts')
-        # blocks
-        self._blocks = [
-            revml.contract.layers.DecoderBlock(
-                num_heads=num_heads,
-                embed_dim=embed_dim,
-                head_dim=head_dim,
-                hidden_dim=hidden_dim,
-                sequence_axis=1,
-                epsilon=epsilon,
-                name='block-{}'.format(__i))
-            for __i in range(num_layers)]
-        # 8 bits for each input byte
-        self._project = tf.keras.layers.Dense(units=8 * input_dim, activation='sigmoid', use_bias=False, kernel_initializer='glorot_uniform', bias_initializer='zeros', name='project')
+        # layers
+        self._encoder_input = None
+        self._encoder_context = None
+        self._transformer = []
+        self._decoder = None
 
-    def build(self, inputs_shape: tf.TensorShape) -> None:
-        __inputs_shape, __contexts_shape = inputs_shape
-        __inputs_shape, __contexts_shape = list(__inputs_shape), list(__contexts_shape)
+    def build(self, input_shape: tuple) -> None:
+        __inputs_shape, __contexts_shape = list(input_shape[0]), list(input_shape[-1])
+        # the inputs is always UTF-32-BE bytes => 256
+        self._encoder_input = mlable.layers.embedding.TokunEmbedding(input_dim=256, output_dim=self._config['embed_dim'] // self._config['input_dim'], name='encoder-input')
+        self._encoder_context = mlable.layers.embedding.TokunEmbedding(input_dim=256, output_dim=self._config['embed_dim'] // self._config['context_dim'], name='encoder-context')
+        # blocks
+        self._transformer = [
+            revml.contract.layers.DecoderBlock(
+                attention_axes=[1],
+                head_num=self._config['head_num'],
+                expand_rate=self._config['expand_rate'],
+                dropout_rate=self._config['dropout_rate'],
+                epsilon=self._config['epsilon'],
+                name='block-{}'.format(__i))
+            for __i in range(self._config['layer_num'])]
+        # 8 bits for each input byte
+        self._decoder = tf.keras.layers.Dense(units=8 * self._config['input_dim'], activation='sigmoid', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', name='decoder')
         # the embeddings are entirely defined in the constructor
-        self._embed_input.build(__inputs_shape)
-        self._embed_context.build(__contexts_shape)
+        self._encoder_input.build(__inputs_shape)
+        self._encoder_context.build(__contexts_shape)
         # both inputs and contexts have the same feature dimension after embedding
         __inputs_shape[-1] = self._config['embed_dim']
         __contexts_shape[-1] = self._config['embed_dim']
         # propagate the shapes through the child layers
-        for __b in self._blocks: __b.build(inputs_shape=__inputs_shape, contexts_shape=__contexts_shape)
-        self._project.build(__inputs_shape)
+        for __b in self._transformer: __b.build(inputs_shape=__inputs_shape, contexts_shape=__contexts_shape)
+        self._decoder.build(__inputs_shape)
         # register
         self.built = True
 
-    def call(self, inputs: tuple, attention_mask: tf.Tensor=None, **kwargs) -> tf.Tensor:
+    def call(self, inputs: tuple, attention_mask: tf.Tensor=None, training: bool=False, **kwargs) -> tf.Tensor:
         # unpack
         __inputs, __contexts = inputs
         # embed
-        __y = self._embed_input(__inputs)
-        __c = self._embed_context(__contexts)
+        __y = self._encoder_input(__inputs)
+        __c = self._encoder_context(__contexts)
         # blocks
-        __y = functools.reduce(lambda __x, __b: __b(inputs=__x, contexts=__c, attention_mask=attention_mask, **kwargs), self._blocks, __y)
+        __y = functools.reduce(lambda __x, __b: __b(inputs=__x, contexts=__c, attention_mask=attention_mask, training=training, **kwargs), self._transformer, __y)
         # decompress
-        return self._project(__y)
+        return self._decoder(__y)
 
     def get_config(self) -> dict:
         __config = super(Transformer, self).get_config()
